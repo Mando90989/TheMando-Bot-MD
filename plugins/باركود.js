d2356f60bbaf79ba98ae5c8ebe0487d1aeda55b0
Mando90989 <fyyuose@gmail.com>
2024-10-28 20:10:56 +0300

    Add files via upload

--- plugins/باركود.js
+++ plugins/باركود.js
@@ -1,7 +1,7 @@
 import { toDataURL } from 'qrcode'
 let handler = async (m, { text, conn }) => {
-if (!text) throw `*حط الحاجه الي عايز تحولها كود*`
-conn.sendFile(m.chat, await toDataURL(text.slice(0, 2048), { scale: 8 }), 'qrcode.png', '¯\\_(BY:Naruto-zack', m)
+if (!text) throw `*اكتب كلام الي تبغاه يحول الى باركود 🧸*`
+conn.sendFile(m.chat, await toDataURL(text.slice(0, 2048), { scale: 8 }), 'qrcode.png', '¯\\_(BY:HODA_AND_MAHDI', m)
 }
 handler.help = ['', 'code'].map(v => 'qr' + v + ' <teks>')
 handler.tags = ['tools']
--- plugins/تطبيق.js
+++ plugins/تطبيق.js
@@ -1,18 +1,38 @@
 import {search, download} from 'aptoide-scraper';
-const handler = async (m, {conn, usedPrefix: prefix, command, text}) => {
- if (!text) throw `*֎╎اكـتـب اسـم تـطـبـيـق لـتـنـزيـلـه❤️‍🩹*`;
+
+const handler = async (m, {conn, usedPrefix, command, text}) => {
+  if (!text) return conn.reply(m.chat, `⚠️ *اكتب اسم التطبيق (APK)*`, m, {contextInfo: {externalAdReply: {mediaUrl: null, mediaType: 1, description: null, body: `ماذا تبحث؟`, previewType: 0, thumbnail: img.getRandom(), sourceUrl: redes.getRandom()}}});
+  
   try {    
     const searchA = await search(text);
     const data5 = await download(searchA[0].id);
-    let response = `*〖📲┇تـحـمـيـل الـتـطـبـيـقـات┇📲〗*\n\n֎╎اسـم الـتـطبـيـق📌┇ ${data5.name}\n֎╎اسـم الـطـرد📦┇ ${data5.package}\n֎╎الانـتـاج🕒┇ ${data5.lastup}\n֎╎الـحـجـم📥┇ ${data5.size}`
-    await conn.sendMessage(m.chat, {image: {url: data5.icon}, caption: response}, {quoted: m});
- if (data5.size.includes('GB') || data5.size.replace(' MB', '') > 999) {
-      return await conn.sendMessage(m.chat, {text: '*֎╎حـجـم الـمـلـف كـبـيـر جـدا لـذا لـن يـتـم ارسـالـه⛔*'}, {quoted: m});
+    let response = `≪*تم التنزيل🧚🏼‍♂️*≫
+
+┏━━━━━━━━━━━━━━━━━━━━━━• 
+┃💫 *لايف*: ${data5.name}
+┃📦 *باكج*: ${data5.package}
+┃🕒 *اخر تحديث*: ${data5.lastup}
+┃💪 *حجم*: ${data5.size}
+┗━━━━━━━━━━━━━━━━━━━━━━━•`;
+
+    await conn.sendFile(m.chat, data5.icon, 'apk.jpg', response, m, false, {contextInfo: {externalAdReply: {mediaUrl: null, mediaType: 1, description: null, body: iig, previewType: 0, thumbnail: img.getRandom(), sourceUrl: redes.getRandom()}}});
+    
+    if (data5.size.includes('GB') || parseFloat(data5.size.replace(' MB', '')) > 999) {
+      return await conn.sendMessage(m.chat, {text: '*التطبيق كبير جداً.*'}, {quoted: m});
     }
-    await conn.sendMessage(m.chat, {document: {url: data5.dllink}, mimetype: 'application/vnd.android.package-archive', fileName: data5.name + '.apk', caption: null}, {quoted: m});
-  } catch {
-    throw `֎╎ايـرور❗`;
-  }    
+    
+    await conn.sendMessage(m.chat, {document: {url: data5.dllink}, mimetype: 'application/vnd.android.package-archive', fileName: data5.name + '.apk', caption: null}, {quoted: m}); 
+    handler.limit = 2;
+  } catch (e) {
+    m.react(`❌`); 
+    console.log(e);
+    handler.limit = false;
+  }
 };
-handler.command = /^(apkmod|apk|apek|تطبيق)$/i;
+
+handler.help = ['apk', 'apkmod'];
+handler.tags = ['downloader'];
+handler.command = /^(تطبيق|apk|modapk|dapk2|aptoide|aptoidedl)$/i;
+handler.register = true;
+
 export default handler;
